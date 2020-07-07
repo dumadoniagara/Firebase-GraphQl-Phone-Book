8c05ce20cab2ad3c668d5d953dbf2d6b6407084a
dumadoniagara <dumadsambora@gmail.com>
2020-07-07 21:01:38 +0700

    testing firebase CRU

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,7 +5,7 @@ const firebase = require("firebase");
 router.get('/', function (req, res, next) {
   const phoneReference = firebase.database().ref("/Phones/");
   phoneReference.on("value", function (snapshot) {
-    if(!snapshot.val()){
+    if (!snapshot.val()) {
       console.log('Not yet data in database')
     }
     res.json(snapshot.val());
@@ -21,13 +21,28 @@ router.post('/', function (req, res) {
   const { name, phone } = req.body;
   const referencePath = `/Phones/${id}/`;
   const phoneReference = firebase.database().ref(referencePath);
-  phoneReference.set({ name: name, phone: phone }, function(error){
-    if(error){
+  phoneReference.set({ name: name, phone: phone }, function (error) {
+    if (error) {
       res.send("Data could not be saved." + error)
     } else {
       res.send("Data saved successfully.");
     }
   });
 });
+
+router.put('/:id', function (req, res) {
+  const { name, phone } = req.body;
+  const id = req.params.id;
+
+  const referencePath = `/Phones/${id}/`;
+  const phoneReference = firebase.database().ref(referencePath);
+  phoneReference.update({ name: name, phone: phone }, function(error){
+    if(error){
+      res.send("Data could not be updated" + error)
+    } else {
+      res.send("Data updated successfully.");
+    }
+  });
+})
 
 module.exports = router;
