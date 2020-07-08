31f9d42ac7ecd7784ee9dc73dedd8e5067d665ee
dumadoniagara <dumadsambora@gmail.com>
2020-07-08 17:47:30 +0700

    type phone done

---
+++ server/graphql/types/phone.js
@@ -0,0 +1,18 @@
+const {GraphQLObjectType, GraphQLNonNull, GraphQLID, GraphQLString, GraphQLInt} = require('graphql');
+
+exports.phoneType = new GraphQLObjectType({
+  name : 'phone',
+  fields: function(){
+   return {
+      id : {
+         type: new GraphQLNonNull(GraphQLID)
+      },
+      name:{
+         type: GraphQLString
+      },
+      phone:{
+         type: GraphQLInt
+      }
+   }
+  } 
+})
