e2a57d1f180977c1e1d43a4545d2d561957e3f5d
dumadoniagara <dumadsambora@gmail.com>
2020-07-09 10:41:25 +0700

    list contact

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -5,3 +5,37 @@ const API_URL = 'http://localhost:3001/graphql/';
 const client = new ApolloClient({
    uri: API_URL
 });
+
+// start load phones
+export const loadPhonesSuccess = (phones) => ({
+   type: 'LOAD_COMMENT_SUCCESS',
+   phones
+})
+
+export const loadPhonesFailure = () => ({
+   type: 'LOAD_COMMENT_FAILURE'
+})
+
+export const loadPhones = () => {
+   const phonesQuery = gql`
+   query {
+      phones{
+         id
+         name
+         phone
+      }
+    }`;
+   return dispatch => {
+      return client.query({
+         query: phonesQuery
+      }).then(function(response){
+         console.log(response);
+         dispatch(loadPhonesSuccess(response.data.phones))
+      })
+      .catch(function(error){
+         console.log(error);
+         dispatch(loadPhonesFailure())
+      })
+   }
+}
+
---
+++ client/src/components/Contact.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Contact = (props) => {
+   return (
+      <div className="contact-item">
+         <table class="table">
+            <thead class="thead-dark">
+               <tr>
+                  <th scope="col">#</th>
+                  <th scope="col">Name</th>
+                  <th scope="col">Phone</th>
+                  <th scope="col">Actions</th>
+               </tr>
+               <tbody>
+                  <tr>
+                     <th scope="row"></th>
+                     <td></td>
+                     <td></td>
+                     <td></td>
+                  </tr>
+               </tbody>
+            </thead>
+         </table>
+      </div>
+   )
+}
